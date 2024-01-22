e9ce83518b51c8da29278efcf11dfba8d3f8f7c5
antonindrawan <anton.indrawan@gmail.com>
2024-01-22 15:01:38 +0100

    feat: add a base module
    Wizard: New -> Module -> Kotlin Multiplatform Shared Module

--- base/src/commonMain/kotlin/org/anton/learncmp/base/Platform.kt
+++ base/src/commonMain/kotlin/org/anton/learncmp/base/Platform.kt
@@ -1,3 +1,5 @@
+package org.anton.learncmp.base
+
 interface Platform {
     val name: String
 }
--- base/src/iosMain/kotlin/org/anton/learncmp/base/Platform.ios.kt
+++ base/src/iosMain/kotlin/org/anton/learncmp/base/Platform.ios.kt
@@ -1,3 +1,5 @@
+package org.anton.learncmp.base
+
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {

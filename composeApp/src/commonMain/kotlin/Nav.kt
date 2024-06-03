c281712777b6ed3ed1b15a487bfee1ecbdee4334
antonindrawan <anton.indrawan@gmail.com>
2024-06-03 17:04:38 +0200

    feat: add svg asset in button

--- composeApp/src/commonMain/kotlin/Nav.kt
+++ composeApp/src/commonMain/kotlin/Nav.kt
@@ -1,7 +1,15 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import components.ButtonExamples
+import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 import moe.tlaster.precompose.navigation.NavHost
 import moe.tlaster.precompose.navigation.Navigator
@@ -32,6 +40,16 @@ fun Nav() {
                 },
             ) {
                 Text(stringResource(resource = MR.strings.show_button_example))
+
+                Image(
+                    painter = painterResource(MR.images.sailboat_svgrepo_com),
+                    contentDescription = null,
+                    contentScale = ContentScale.Inside,
+                    modifier =
+                        Modifier
+                            .size(30.dp)
+                            .clip(RoundedCornerShape(4.dp)),
+                )
             }
         }
 

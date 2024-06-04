4189f6a10e4fbfb4fac464d1e74ecbda43aee761
antonindrawan <anton.indrawan@gmail.com>
2024-06-04 16:52:48 +0200

    feat: add an image to the jvm app

--- composeApp/src/desktopMain/kotlin/Main.kt
+++ composeApp/src/desktopMain/kotlin/Main.kt
@@ -1,14 +1,39 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import dev.icerock.moko.resources.compose.painterResource
+import org.anton.learncmp.resources.MR
 
 fun main() =
     application {
         Window(onCloseRequest = ::exitApplication, title = "LearnComposeMP") {
-            App()
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                MainView() // adding a picture on jvm
+                App()
+            }
         }
     }
+
+@Composable
+fun MainView() {
+
+    Image(
+        painter = painterResource(MR.images.sailboat_svgrepo_com),
+        contentDescription = "Just an image from JVM",
+        modifier = Modifier
+            .size(50.dp)
+            .clip(RoundedCornerShape(4.dp)),
+    )
+}
 
 @Preview
 @Composable
